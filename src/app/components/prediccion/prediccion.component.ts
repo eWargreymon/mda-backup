2d1967105924b5c5cdfda31217df4ff30b7c5091
Álvaro <alvarosn_98@hotmail.com>
2021-04-27 13:25:05 +0100

    Add observable

--- src/app/components/prediccion/prediccion.component.ts
+++ src/app/components/prediccion/prediccion.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Flower, FlowerServiceService } from 'src/app/services/flower-service.service';
 import { FloresComponent } from '../flores/flores.component';
-
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+ 
 @Component({
   selector: 'app-prediccion',
   templateUrl: './prediccion.component.html',
@@ -11,8 +14,11 @@ export class PrediccionComponent implements OnInit {
   
   imagen: any;
   flores: Flower[];
+  arrayFlores: Flower[];
   predictedName: string;
   predictedFlower: Flower;
+  flowerList: AngularFirestoreCollection<Flower>;
+  observableFlores: Observable<Flower[]>;
   
   constructor(private flowerService: FlowerServiceService) { 
     this.flores = []; 
@@ -26,6 +32,12 @@ export class PrediccionComponent implements OnInit {
       );
     });
 
+    this.observableFlores = this.flowerService.getList();
+    
+    this.observableFlores.subscribe(
+      (x: any) => this.arrayFlores = x,
+    );
+
     this.imagen=localStorage.getItem('img');
 
     this.predictedName = this.predict(this.imagen);
@@ -38,6 +50,5 @@ export class PrediccionComponent implements OnInit {
   predict(img: string): string{
     return null;
   }
-
 
 }
--- src/app/services/flower-service.service.ts
+++ src/app/services/flower-service.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlowerServiceService {
 
+  flowerList: AngularFirestoreCollection<Flower>;
+
   constructor(private readonly afs: AngularFirestore) { }
 
   getAll(){
     return this.afs.collection('Flowers').snapshotChanges();
+  }
+
+  getList(){
+    return this.flowerList.snapshotChanges().pipe(
+      map(actions => actions.map(a => a.payload.doc.data() as Flower))
+      );
   }
 
   addFlower(payload: Flower){
